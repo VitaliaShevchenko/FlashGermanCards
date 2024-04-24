2a69c7255cfe6b0ee59b942f25e8787b006c8fe8
VitaliaShevchenko <103375210+VitaliaShevchenko@users.noreply.github.com>
2024-04-24 15:55:19 +0200

    style cards with all words

--- scripts/all-words.js
+++ scripts/all-words.js
@@ -5,13 +5,20 @@ function renderAllWords(wordsArray) {
   for(let i = 0; i < wordsArray.length; i++) {
     const wordEl = document.createElement('div');
     wordEl.classList.add('word-container');
-    wordEl.textContent = `
-    ${wordsArray[i].german}
-    ${wordsArray[i].english}
-    ${wordsArray[i].example}
-    `;
+    const germanEl = document.createElement('p')
+    germanEl.classList.add('german-word');
+    germanEl.textContent = wordsArray[i].german;
+    wordEl.append(germanEl);
+    const englishEl = document.createElement('p')
+    englishEl.classList.add('english-word');
+    englishEl.textContent = wordsArray[i].english;
+    wordEl.append(englishEl);
+    const exampleEl = document.createElement('p')
+    exampleEl.classList.add('example-line');
+    exampleEl.textContent = wordsArray[i].example;
+    wordEl.append(exampleEl);
     allWordsContainer.append(wordEl);
   }
 }
-renderAllWords(wordsA1);
-renderAllWords(wordsA2);
+
+renderAllWords(wordsA1)
